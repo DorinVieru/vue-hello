ecf2cd1c982c049a675d1abed333ef8e4f54bd12
DorinVieru <dorino.vieru@gmail.com>
2023-12-11 15:30:39 +0100

    [UPDATE] Basic setting of html & js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// DICHIARO VARIABILE DEL METODO createApp
+const { createApp } = Vue;
+
+// INIZIALIZZAZIONE createApp E CREAZIONE ISTANZA DELL'APPLICAZIONE VUE
+createApp({
+    // DEFINISCO IL METODO data () NEL QUALE INSERIRO' UN RETURN
+    data(){
+        return {
+            
+        }
+    } 
+})
